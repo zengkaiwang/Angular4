a8ca9322971428e0d3829240272a5dab512ca6ff
王增凯 <zengkai.wang@irootech.com>
2018-07-13 16:13:34 +0800

    应用typescript语法开发公共模块sessionStorage.ts辅助操作session

--- angular-project/src/app/app.component.ts
+++ angular-project/src/app/app.component.ts
@@ -6,6 +6,8 @@ import './widget/script/string';
 import './widget/script/base64.js';
 declare var Base64: any;
 
+import { session } from './widget/script/sessionStorage'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -17,11 +19,17 @@ export class AppComponent {
   private str: string = 'abc'
 
   ngOnInit() {
+    //测试Date对象上拓展的方法
   	let newStr =  this.str.padLeft('w', 9)
   	console.log('newStr', newStr);
 
+    //测试第三方js文件引入
     let base64 = new Base64();
     let password = base64.encode('123456'); //对密码加密
     console.log('password', password)
+
+    //测试应用ts开发的公共模块
+    session.set('keyaaa', "value");
+    console.log(session.get('keyaaa'));
   }
 }
