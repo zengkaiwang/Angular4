7cc197ddfb5f0e18aef9e910dec3647dde81c0f4
王增凯 <zengkai.wang@irootech.com>
2018-07-13 15:51:45 +0800

    第三方的非node依赖包在angular5中的使用

--- angular-project/src/app/app.component.ts
+++ angular-project/src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 
+import './widget/script/date';
 import './widget/script/string';
+
+import './widget/script/base64.js';
+declare var Base64: any;
 
 @Component({
   selector: 'app-root',
@@ -15,5 +19,9 @@ export class AppComponent {
   ngOnInit() {
   	let newStr =  this.str.padLeft('w', 9)
   	console.log('newStr', newStr);
+
+    let base64 = new Base64();
+    let password = base64.encode('123456'); //对密码加密
+    console.log('password', password)
   }
 }
