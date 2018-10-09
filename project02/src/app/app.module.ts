22289e07ea59a684f4bcf12f1e8561153494c11c
zengkaiwang <zengkaiwang@yeah.net>
2018-10-09 23:09:24 +0800

    项目中引入ng-zorro-antd@0.5

--- project02/src/app/app.module.ts
+++ project02/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgZorroAntdModule } from 'ng-zorro-antd'
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgZorroAntdModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
