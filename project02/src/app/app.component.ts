1be2a7346c15e2eebbcda8fdcf719a13024fbc81
zengkaiwang <zengkaiwang@yeah.net>
2018-10-11 00:18:42 +0800

    框架中使用第三方js文件以及在String上拓展新方法

--- project02/src/app/app.component.ts
+++ project02/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import './widget/script/string';
+import './widget/script/base64.js';
+declare var Base64: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  private str: string = "abc"
+  ngOnInit() {
+  	//测试base64.js
+		let base64 = new Base64();
+		let password = base64.encode('123456'); //对密码加密
+		console.log('password', password);
+
+		//测试String对象上拓展的方法
+		let newstr = this.str.padLeft('w',9);
+		console.log('newstr', newstr);
+  }
 }
